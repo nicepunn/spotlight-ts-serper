d65ee16ff7d8deacffda2c435a242a6a7fb1f601
Punnawat Lohanuit <punnawat58000@gmail.com>
2024-06-06 18:50:15 +0700

    feat: cancopy

--- src/app/serperMock/auth/api-key/page.tsx
+++ src/app/serperMock/auth/api-key/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 import { Button } from '@/components/Button'
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/20/solid'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useApiKeyStore } from '../../zustand/store'
+import { useForm } from 'react-hook-form'
 
 export default function APIKey() {
   return (
@@ -21,10 +23,30 @@ export default function APIKey() {
 
 function MyCard() {
   const [copyState, setCopyState] = useState<'Copy' | 'Copied'>('Copy')
+  useEffect(() => {
+    if (copyState === 'Copied') {
+      const timer = setTimeout(() => {
+        setCopyState('Copy')
+      }, 2000)
+      return () => clearTimeout(timer)
+    }
+  }, [copyState])
   const [isKeyVisible, setKeyVisible] = useState(false)
+  const apiKey = useApiKeyStore((state) => state.apiKey)
+
+  const { register, handleSubmit, control, watch } = useForm({
+    defaultValues: {
+      apiKey: apiKey,
+    },
+  })
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
   return (
     <div className="h-[320px] w-full max-w-[448px] rounded-lg bg-zinc-100 shadow dark:bg-zinc-900">
-      <div className="p-8">
+      <form onSubmit={handleSubmit(onSubmit)} className="p-8">
         <div className="flex w-full flex-col gap-y-4">
           <div className="flex w-full flex-row gap-x-2">
             <label htmlFor="email" className="sr-only">
@@ -32,7 +54,8 @@ function MyCard() {
             </label>
             <div className="relative h-fit w-full">
               <input
-                value={'2aa1f782fa840f29ef0629249d621449d7'}
+                {...register('apiKey')}
+                value={apiKey}
                 type={isKeyVisible ? 'text' : 'password'}
                 name="your key"
                 id="your key"
@@ -51,24 +74,51 @@ function MyCard() {
                 />
               )}
             </div>
-            <Button
-              onClick={() => {
-                setCopyState('Copied')
-                const timer = setTimeout(() => {
-                  setCopyState('Copy')
-                }, 2000)
-              }}
-              className="px-4"
-              variant="secondary"
-            >
-              {copyState}
-            </Button>
+            <CopyButton
+              watch={watch}
+              copyState={copyState}
+              setCopyState={setCopyState}
+            />
           </div>
           <Button className="w-fit px-4" variant="primary">
             Reset key
           </Button>
         </div>
-      </div>
+      </form>
     </div>
+  )
+}
+
+function CopyButton(props: { watch: any; copyState: any; setCopyState: any }) {
+  const cutFirstAndLastLetter = (str: string) => {
+    if (str.length <= 2) {
+      return ''
+    }
+    return str.slice(1, -1)
+  }
+  const { copyState, setCopyState, watch } = props
+  const handleCopy = () => {
+    const formData = watch('apiKey')
+    const formattedData = cutFirstAndLastLetter(
+      JSON.stringify(formData, null, 2),
+    )
+
+    navigator.clipboard.writeText(formattedData).then(() => {
+      setCopyState('Copied')
+      setTimeout(() => {
+        setCopyState('Copy')
+      }, 2000)
+    })
+  }
+
+  return (
+    <Button
+      type="button"
+      onClick={handleCopy}
+      className="px-4"
+      variant="secondary"
+    >
+      {copyState}
+    </Button>
   )
 }
